2ce37f69f4ab530f5ecd7946fea74035f23f0b4d
Remo Fritzsche <remo.fritzsche@sitrox.com>
2017-12-13 11:51:16 +0100

    Comply with rubocop

--- lib/generators/workhorse/templates/config/initializers/workhorse.rb
+++ lib/generators/workhorse/templates/config/initializers/workhorse.rb
@@ -1,11 +1,11 @@
 Workhorse.setup do |config|
- # Set this to false in order to prevent jobs from being automatically
- # wrapped into a transaction. The built-in workhorse logic will still run
- # in transactions.
- config.perform_jobs_in_tx = true
+  # Set this to false in order to prevent jobs from being automatically
+  # wrapped into a transaction. The built-in workhorse logic will still run
+  # in transactions.
+  config.perform_jobs_in_tx = true
 
- # Enable this to specify an alternative callback for handling transactions.
- # config.tx_callback = proc do |&block|
- #   ActiveRecord::Base.transaction&(&block)
- # end
+  # Enable this to specify an alternative callback for handling transactions.
+  # config.tx_callback = proc do |&block|
+  #   ActiveRecord::Base.transaction&(&block)
+  # end
 end
