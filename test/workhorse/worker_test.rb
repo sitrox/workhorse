4c460d7482372b29511104f331948ae03f50e12b
Remo Fritzsche <remo.fritzsche@sitrox.com>
2017-12-14 11:56:37 +0100

    Test worker queues

--- test/workhorse/worker_test.rb
+++ test/workhorse/worker_test.rb
@@ -76,4 +76,52 @@ class Workhorse::WorkerTest < ActiveSupport::TestCase
     sleep 1
     w.assert_state! :shutdown
   end
+
+  def test_no_queues
+    enqueue_in_multiple_queues
+    work 2, queues: [nil, :q1]
+
+    jobs = Workhorse::DbJob.all.to_a
+    assert_equal 'succeeded', jobs[0].state
+    assert_equal 'succeeded', jobs[1].state
+    assert_equal 'waiting',   jobs[2].state
+  end
+
+  def test_queues_with_nil
+    enqueue_in_multiple_queues
+    work 2, queues: [nil, :q1]
+
+    jobs = Workhorse::DbJob.all.to_a
+    assert_equal 'succeeded', jobs[0].state
+    assert_equal 'succeeded', jobs[1].state
+    assert_equal 'waiting',   jobs[2].state
+  end
+
+  def test_queues_without_nil
+    enqueue_in_multiple_queues
+    work 2, queues: %i[q1 q2]
+
+    jobs = Workhorse::DbJob.all.to_a
+    assert_equal 'waiting',   jobs[0].state
+    assert_equal 'succeeded', jobs[1].state
+    assert_equal 'succeeded', jobs[2].state
+  end
+
+  private
+
+  def work(time = 2, options = {})
+    options[:pool_size] ||= 5
+    options[:polling_interval] ||= 1
+
+    w = Workhorse::Worker.new(options)
+    w.start
+    sleep time
+    w.shutdown
+  end
+
+  def enqueue_in_multiple_queues
+    Workhorse::Enqueuer.enqueue BasicJob.new(some_param: nil)
+    Workhorse::Enqueuer.enqueue BasicJob.new(some_param: :q1), queue: :q1
+    Workhorse::Enqueuer.enqueue BasicJob.new(some_param: :q2), queue: :q2
+  end
 end
