67848561f8c3d5bc02138517b4d9a156cfb51bf5
Remo Fritzsche <remo.fritzsche@sitrox.com>
2017-12-14 11:01:47 +0100

    Optimize table creation migration

--- lib/generators/workhorse/templates/create_table_jobs.rb
+++ lib/generators/workhorse/templates/create_table_jobs.rb
@@ -1,9 +1,9 @@
 class CreateTableJobs < ActiveRecord::Migration
   def change
-    create_table :jobs do |t|
+    create_table :jobs, force: true do |t|
       t.string :state, null: false, default: 'waiting'
       t.string :queue, null: true
-      t.text :handler, null: false
+      t.text :handler, null: false, limit: 4_294_967_295
 
       t.string :locked_by
       t.datetime :locked_at
@@ -12,9 +12,12 @@ class CreateTableJobs < ActiveRecord::Migration
 
       t.datetime :succeeded_at
       t.datetime :failed_at
-      t.text :last_error
+      t.text :last_error, limit: 4_294_967_295
 
       t.timestamps null: false
     end
+
+    add_index :jobs, :queue
+    add_index :jobs, :state
   end
 end
