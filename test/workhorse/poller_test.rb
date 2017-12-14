f572853bb179a2f440910d000879ed51b53a778f
Remo Fritzsche <remo.fritzsche@sitrox.com>
2017-12-14 13:18:06 +0100

    Add basic poller test

---
+++ test/workhorse/poller_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class Workhorse::PollerTest < WorkhorseTest
+  def test_interruptable_sleep
+    w = Workhorse::Worker.new(polling_interval: 60)
+    w.start
+    sleep 2
+
+    Timeout.timeout(1.5) do
+      w.shutdown
+    end
+  end
+end
