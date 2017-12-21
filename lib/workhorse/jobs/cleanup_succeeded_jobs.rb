70e1f9c97455f32d0add4c8a881f66eb9c878994
Jan Tschopp <jan.tschopp@sitrox.com>
2017-12-21 11:52:14 +0100

    Add stock cleanup job

---
+++ lib/workhorse/jobs/cleanup_succeeded_jobs.rb
@@ -0,0 +1,24 @@
+module Workhorse::Jobs
+  class CleanupSucceededJobs
+    # Instantiates a new cleanup job
+    #
+    # @param max_age [Integer] The maximal age of jobs to retain, in days. Will
+    #   be evaluated at perform time.
+    def initialize(max_age: 14)
+      @max_age = max_age
+    end
+
+    def perform
+      age_limit = seconds_ago(@max_age)
+      Workhorse::DbJob.where(
+        'STATE = ? AND UPDATED_AT <= ?', Workhorse::DbJob::STATE_SUCCEEDED, age_limit
+      ).delete_all
+    end
+
+    private
+
+    def seconds_ago(days)
+      Time.now - days * 24 * 60 * 60
+    end
+  end
+end
