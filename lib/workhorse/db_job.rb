cc186e17fc67cd2a812c3ca06380ba4b515d5273
Remo Fritzsche <remo.fritzsche@sitrox.com>
2019-05-14 11:29:52 +0200

    Add API and doc for querying / resetting db jobs
    This adresses issue #16 and provides a way for querying database jobs in
    specific states and resetting them back to state `waiting` using the
    `reset!` method. The new method is covered by a new unit test.
    
    The README gets a new section explaining how to obtain and manipulate
    database jobs.

--- lib/workhorse/db_job.rb
+++ lib/workhorse/db_job.rb
@@ -12,6 +12,58 @@ module Workhorse
 
     self.table_name = 'jobs'
 
+    def self.waiting
+      where(state: STATE_WAITING)
+    end
+
+    def self.locked
+      where(state: STATE_LOCKED)
+    end
+
+    def self.started
+      where(state: STATE_STARTED)
+    end
+
+    def self.succeeded
+      where(state: STATE_SUCCEEDED)
+    end
+
+    def self.failed
+      where(state: STATE_FAILED)
+    end
+
+    # Resets job to state "waiting" and clears all meta fields
+    # set by workhorse in course of processing this job.
+    #
+    # This is only allowed if the job is in a "dead-end-state" ("succeeded",
+    # "failed"), as it is safe to do so because workhorse won't touch these
+    # jobs. Set "force" to true in order to reset the job without checking for
+    # its state. If you do so, make sure that the job is not still being
+    # processed by a worker. If possible, shutdown all workers before performing
+    # a forced reset.
+    #
+    # After the job is reset, it will be performed again. If you reset a job
+    # that has already been performed ("succeeded") or partially performed
+    # ("failed"), make sure the actions performed in the job are repeatable or
+    # have been rolled back. E.g. if the job already wrote something to an
+    # external API, it may cause inconsistencies if the job is performed again.
+    def reset!(force = false)
+      unless force
+        assert_state! STATE_SUCCEEDED, STATE_FAILED
+      end
+
+      self.state = STATE_WAITING
+      self.locked_at = nil
+      self.locked_by = nil
+      self.started_at = nil
+      self.succeeded_at = nil
+      self.failed_at = nil
+      self.last_error = nil
+
+      save!
+    end
+
+    # @private Only to be used by workhorse
     def mark_locked!(worker_id)
       if changed?
         fail "Dirty jobs can't be locked."
@@ -27,6 +79,7 @@ module Workhorse
       save!
     end
 
+    # @private Only to be used by workhorse
     def mark_started!
       assert_state! STATE_LOCKED
 
@@ -35,6 +88,7 @@ module Workhorse
       save!
     end
 
+    # @private Only to be used by workhorse
     def mark_failed!(exception)
       assert_state! STATE_LOCKED, STATE_STARTED
 
@@ -44,6 +98,7 @@ module Workhorse
       save!
     end
 
+    # @private Only to be used by workhorse
     def mark_succeeded!
       assert_state! STATE_STARTED
 
---
+++ test/workhorse/db_job_test.rb
@@ -0,0 +1,58 @@
+require 'test_helper'
+
+class Workhorse::DbJobTest < WorkhorseTest
+  def test_reset_succeeded
+    job = Workhorse.enqueue(BasicJob.new(sleep_time: 0))
+    work 0.5
+    job.reload
+    assert_equal 'succeeded', job.state
+
+    job.reset!
+
+    assert_clean job
+  end
+
+  def test_reset_failed
+    job = Workhorse.enqueue FailingTestJob
+    work 0.5
+    job.reload
+    assert_equal 'failed', job.state
+
+    job.reset!
+
+    assert_clean job
+  end
+
+  def test_reset_locked_unforced
+    job = Workhorse.enqueue(BasicJob.new(sleep_time: 0))
+    job.mark_locked!(42)
+
+    err = assert_raises do
+      job.reset!
+    end
+    assert_equal %(Job #{job.id} is not in state [:succeeded, :failed] but in state "locked".), err.message
+  end
+
+  def test_forced_reset
+    job = Workhorse.enqueue(BasicJob.new(sleep_time: 0))
+    job.mark_locked!(42)
+
+    assert_nothing_raised do
+      job.reset!(true)
+    end
+
+    assert_clean job
+  end
+
+  private
+
+  def assert_clean(job)
+    assert_equal 'waiting', job.state
+    assert_nil job.locked_by
+    assert_nil job.locked_at
+    assert_nil job.started_at
+    assert_nil job.failed_at
+    assert_nil job.succeeded_at
+    assert_nil job.last_error
+  end
+end
